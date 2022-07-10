720da03436371353f0b01342fdac4346eeed4e0f
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 23:17:31 +0300

    Sukurta ketvirtoss uzduoties failass

---
+++ src/Uzduotis4.java
@@ -0,0 +1,14 @@
+/*
+Užduotis 4:
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurkite papildomą funkciją, kuri patikrina ar skaičius yra neigiamas.
+3. Susikurti papildomą funkciją, kuri gauna int[] masyvas ir visus neigiamus skaičius, naudojant Math.abs() konvertuotų į priešingus ir išsaugotų tai masyve. T.y., jei funkcija gauna masyvas = {3, -24, 8, 4, -7, 3, -21} tai po funkcijos masyvas turėtų būti masyvas = {3, 24, 8, 4, 7, 3, 21}
+
+ */
+public class Uzduotis4 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
