9b7a8fd9aa73ced1fdc02d64e4fc88697e88d357
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 23:18:37 +0300

    Masyvu nusskaitymo funkcija

--- src/Uzduotis4.java
+++ src/Uzduotis4.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
 Užduotis 4:
 1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
@@ -7,8 +10,24 @@ Užduotis 4:
  */
 public class Uzduotis4 {
     public static void main(String[] args) {
+        int[] skaiciai = masyvoNuskaitymas();
+        System.out.println("skaiciai = " + Arrays.toString(skaiciai));
 
     }
 
-
+    /**
+     * Nuskaito skaičių massyvą iš konsolės
+     * @return skaičių masyvą
+     */
+    public static int[] masyvoNuskaitymas() {
+        Scanner skaitytuvas = new Scanner(System.in);
+        System.out.println("Įveskite kiek massyve bus skaičių: ");
+        int n = skaitytuvas.nextInt();
+        int[] skaiciai = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Įveskite " + i + "-ąjį skaičių: ");
+            skaiciai[i] = skaitytuvas.nextInt();
+        }
+        return skaiciai;
+    }
 }
