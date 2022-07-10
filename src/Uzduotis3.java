60766a2453b158f25cc600f085b4b14a13554769
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 23:08:35 +0300

    Sukurtas trecios uzduoties failas

---
+++ src/Uzduotis3.java
@@ -0,0 +1,10 @@
+/**
+ * Užduotis 3:
+ * 1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+ * 2. Susikurti papildomą funkciją, kuri gauna double[] masyvas ir apskaičiuoja jo sumą, tačiau BŪTINAI naudokite while ciklą. Masyvo suma yra gana sena JavaScript uždutis, tačiau pasipraktikuokite while ciklą su paprastesne užduotimi, jog būtų lengviau kitoms (kur būtinai reikės while, nes for neveiks (būtų labai sunku užrašyti for))
+ */
+public class Uzduotis3 {
+    public static void main(String[] args) {
+
+    }
+}
