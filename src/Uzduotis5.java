1843d70d80418ed7b77bb59bcc88f53b7bad8f89
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 23:24:03 +0300

    Sukurtas penktos uzduoties failas

---
+++ src/Uzduotis5.java
@@ -0,0 +1,13 @@
+/**
+ * Užduotis 5:
+ * 1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+ * 2. Susikurti papildomą funkciją, kuri gauna String zodis ir patikrina ar jis yra vyriškos giminės galūnės. t.y. žodžių, kurie baigiasi as, us ar is galūnėmis.
+ * 3. Susikurti papildomą funkciją, kuri gauna String[] masyvas ir patikrintų kiek masyve yra vyriškos giminės žodžių,  Pvz., jeigu turime masyvą zodziai = {"kelmas", "botAGAS", "laumė", "mašina", "burlaivis"}, tai turėtų grąžinti atsakymą 3, kadangi jame yra trys žodžiai, kurie baigiasi vyriška galūn
+ */
+public class Uzduotis5 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
