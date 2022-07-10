3d04490855e09f25b28d3c7b137ada19d1e7324c
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 22:59:42 +0300

    Sukurtas antros uzduoties failass

---
+++ src/Uzduotis2.java
@@ -0,0 +1,11 @@
+/**
+ * Užduotis 2:
+ * 1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+ * 2. Susikurti papildomą funkciją, kuri gauna du int[] masyvus (galima ir listus - su jais lengviau ši užduotis) ir juos sudeda į vieną bendrą didelį ir jį grąžina. T.y., jeigu turime x = {3, 5, 8} ir y = {2, 7, 12, 9}, tai bendras sudėtas masyvas turėtų būti: rezultatas = {3, 5, 8, 2, 7, 12, 9}
+ * PASTABA: NEGALIMA NAUDOTI SUKURTŲ JAVA FUNKCIJŲ. T.y., jokių array.copyOf(), jokių Java Collections, jokių Stream API, jums "sujungimo" funkciją reikia persidaryti patiems. Ko gali reikėti, tai masyvo ilgio, o jį Javoje kaip ir JS masyvas.length
+ */
+public class Uzduotis2 {
+    public static void main(String[] args) {
+
+    }
+}
